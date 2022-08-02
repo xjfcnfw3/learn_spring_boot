fe6f4000bfa6a32ebb4cdab370aa405542fe3a93
kimminsu <xjfcnfw3@naver.com>
2022-08-02 16:16:22 +0900

    HelloResponseDto Test 코드 추가

--- src/test/java/org/example/book/springboot/web/HelloControllerTest.java
+++ src/test/java/org/example/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -24,8 +23,20 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
-        .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        .andExpect(status().isOk()).andExpect(content().string(hello));
 
+    }
+
+    @Test
+    public void return_helloDto() throws Exception{
+        String name = "hello";
+        int amount = 100;
+
+        mvc.perform(get("/hello/dto")
+                .param("name",name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
