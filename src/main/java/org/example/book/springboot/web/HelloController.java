1f17ab93ce332a871551978135ace315b6707644
kimminsu <xjfcnfw3@naver.com>
2022-08-02 16:15:04 +0900

    HelloResponseDto 추가

--- src/main/java/org/example/book/springboot/web/HelloController.java
+++ src/main/java/org/example/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package org.example.book.springboot.web;
 
+import org.example.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,10 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
