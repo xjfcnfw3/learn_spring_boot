48739d661baf697fb0e554520527480210382fde
kimminsu <xjfcnfw3@naver.com>
2022-08-03 21:25:27 +0900

    쿼리 추가

--- src/main/java/org/example/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/book/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,12 @@ package org.example.book.springboot.domain.posts;
 
 import com.sun.org.apache.bcel.internal.generic.L2D;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("select p from Posts p order by  p.id DESC")
+    List<Posts> findAllDesc();
 }
