805cba84b64e2aa4ca8c648f43e763a564b672d8
kimminsu <xjfcnfw3@naver.com>
2022-08-05 12:01:16 +0900

    LoginUser 어노테이션 추가

--- src/main/java/org/example/book/springboot/web/IndexController.java
+++ src/main/java/org/example/book/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.book.springboot.config.auth.LoginUser;
 import org.example.book.springboot.config.auth.dto.SessionUser;
 import org.example.book.springboot.service.PostsService;
 import org.example.book.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user !=null){
             model.addAttribute("userName",user.getName());
